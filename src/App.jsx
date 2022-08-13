031c70f15ecd5d52f29dfcfd9a57b57b7ae4078b
LeonG11 <kkacanov198@gmail.com>
2022-08-13 12:42:56 +0300

    Finish desktop project

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
 import React, { useState } from "react";
+import "./index.css";
+
+function hexGenerator() {
+  return Math.floor(Math.random() * 16777215 + 1).toString(16);
+}
 
 function App() {
   const [colorOne, setColorOne] = useState("ffffff");
-  const [colorTwo, setColorTwo] = useState("ffffff");
+  const [colorTwo, setColorTwo] = useState("000000");
 
   return (
-    <>
-      <div>
+    <div className="container">
+      <div
+        className="hex"
+        style={{
+          background: "linear-gradient(to right,#" + colorOne + ",#" + colorTwo,
+        }}
+      >
         <h1>Нажми для создания случайного градиента с hex цветами</h1>
-        <h3>
+        <h3 >
           background: linear-gradient(to right,#
           {colorOne}
           ,#
-          {colorTwo})
+          {colorTwo});
         </h3>
-        <button>Нажми меня</button>
+        <button
+          onClick={() => {
+            setColorOne(hexGenerator());
+            setColorTwo(hexGenerator());
+          }}
+        >
+          Нажми меня
+        </button>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App;
